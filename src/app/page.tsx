69d24469a0c37fe6b3ee8879832fbca1339e12a7
Wadesemou <semou.wade@uadb.edu.sn>
2023-06-06 13:03:21 +0000

    gdi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,176 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import HTMLFlipBook from "react-pageflip";
+import page1 from "../../public/GDI ADVISORS - Plaquette - VF-03.pdf/1.png";
+import page2 from "../../public/GDI ADVISORS - Plaquette - VF-03.pdf/2.png";
+import page3 from "../../public/GDI ADVISORS - Plaquette - VF-03.pdf/3.png";
+import page4 from "../../public/GDI ADVISORS - Plaquette - VF-03.pdf/4.png";
+import page5 from "../../public/GDI ADVISORS - Plaquette - VF-03.pdf/5.png";
+import page6 from "../../public/GDI ADVISORS - Plaquette - VF-03.pdf/6.png";
+import page7 from "../../public/GDI ADVISORS - Plaquette - VF-03.pdf/7.png";
+import page8 from "../../public/GDI ADVISORS - Plaquette - VF-03.pdf/8.png";
 
-export default function Home() {
+export type Courrier = {
+  courriers: [
+    {
+      titre: "demande de transfere de poste";
+      ref: "009nh2";
+      date: "12/23/2023 ";
+      demandeur: "Diop Pape Demba";
+      destinataire: [
+        {
+          nom: "Semou";
+          prenom: "WADE";
+          email: "semou.wage@gdi-advisors.com";
+        }
+      ];
+    }
+  ];
+};
+export default function MyBook() {
+  const courrier = [
+    {
+      titre: "demande de transfere de poste",
+      ref: "009nh2",
+      date: "12/23/2023 ",
+      demandeur: "Diop Pape Demba",
+      destinataire: [
+        {
+          nom: "Semou",
+          prenom: "WADE",
+          email: "semou.wage@gdi-advisors.com",
+        },
+      ],
+    },
+    {
+      titre: "demande de transfere de poste",
+      ref: "009nh2",
+      date: "12/23/2023 ",
+      demandeur: "Diop Pape Demba",
+      destinataire: [
+        {
+          nom: "Semou",
+          prenom: "WADE",
+          email: "semou.wage@gdi-advisors.com",
+        },
+      ],
+    },
+  ];
+
+  const [dislayfullimg, setDisplayfullimg] = useState(false);
+  const [filter, setfilter] = useState(false);
+
+  const [reference, setReference] = useState("");
+  const [titre, setTitre] = useState("");
+  const [date, setDate] = useState("");
+  const [organisation, setOrganisation] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Faire quelque chose avec les valeurs filtrées, par exemple :
+    console.log("Référence:", reference);
+    console.log("Titre:", titre);
+    console.log("Date:", date);
+    console.log("Organisation:", organisation);
+
+    // Réinitialiser les valeurs du formulaire
+    setReference("");
+    setTitre("");
+    setDate("");
+    setOrganisation("");
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-screen w-full bg-blue-900  ">
+      <HTMLFlipBook
+        width={930}
+        height={968}
+        className=" bg-slate-700 justify-start border-slate-700 mx-auto"
+      >
+        <div className="demoPage relative ">
+          <Image
+            src={page1}
+            width="800"
+            height="500"
+            alt=""
+            className="w-full h-full  border-slate-400 border-r-4"
+          />
+          <div className="absolute right-0 top-0 opacity-70 bg-gradient-to-r from-transparent via-transparent to-gray-900   w-full h-full"></div>
         </div>
-      </div>
+        <div className="demoPage relative ">
+          <Image
+            src={page2}
+            width="800"
+            height="500"
+            alt=""
+            className="w-full h-full  border-slate-400 border-l-4 "
+          />
+          <div className="absolute right-0 top-0 opacity-70 bg-gradient-to-l  border-slate-900 border-l-4 from-transparent via-transparent to-gray-900   w-full h-full"></div>
+        </div>
+        <div className="demoPage relative ">
+          <Image
+            src={page3}
+            width="800"
+            height="500"
+            alt=""
+            className="w-full h-full  border-slate-400 border-r-4"
+          />
+          <div className="absolute right-0 top-0 opacity-70 bg-gradient-to-r from-transparent via-transparent to-gray-900   w-full h-full"></div>
+        </div>
+        <div className="demoPage relative ">
+          <Image
+            src={page4}
+            width="800"
+            height="500"
+            alt=""
+            className="w-full h-full  border-slate-400 border-l-4"
+          />
+          <div className="absolute right-0 top-0 opacity-70 bg-gradient-to-l  border-slate-900 border-l-4 from-transparent via-transparent to-gray-900   w-full h-full"></div>
+        </div>
+        <div className="demoPage relative ">
+          <Image
+            src={page5}
+            width="800"
+            height="500"
+            alt=""
+            className="w-full h-full  border-slate-400 border-r-4"
+          />
+          <div className="absolute right-0 top-0 opacity-70 bg-gradient-to-r from-transparent via-transparent to-gray-900   w-full h-full"></div>
+        </div>
+        <div className="demoPage relative ">
+          <Image
+            src={page6}
+            width="800"
+            height="500"
+            alt=""
+            className="w-full h-full  border-slate-400 border-l-4"
+          />
+          <div className="absolute right-0 top-0 opacity-70 bg-gradient-to-l  border-slate-900 border-l-4 from-transparent via-transparent to-gray-900   w-full h-full"></div>
+        </div>
+        <div className="demoPage relative ">
+          <Image
+            src={page7}
+            width="800"
+            height="500"
+            alt=""
+            className="w-full h-full  border-slate-400 border-r-4"
+          />
+          <div className="absolute right-0 top-0 opacity-70 bg-gradient-to-r from-transparent via-transparent to-gray-900   w-full h-full"></div>
+        </div>
+        <div className="demoPage relative ">
+          <Image
+            src={page8}
+            width="800"
+            height="500"
+            alt=""
+            className="w-full h-full  border-slate-400 border-l-4"
+          />
+          <div className="absolute right-0 top-0 opacity-70 bg-gradient-to-l  border-slate-900 border-l-4 from-transparent via-transparent to-gray-900   w-full h-full"></div>
+        </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        {/* <div className="demoPage  bg-slate-800"></div> */}
+      </HTMLFlipBook>
+    </div>
+  );
 }
